b09bb5ba82b1d83d92f230eb158527054ffe46bb
Reza Khademi <rezakhademix@gmail.com>
2025-03-28 14:42:04 +0330

    update dependecies

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,9 +3,14 @@ return {
         "nvimdev/dashboard-nvim",
         event = "VimEnter",
         opts = function(_, opts)
-            local logo = [[]]
+            local logo = [[
+***
+* NVIM *
+***
+            ]]
+            logo = string.rep("\n", 2) .. logo .. "\n \n"
 
-            logo = string.rep("\n", 8) .. logo .. "\n \n"
+            opts.config = opts.config or {} -- Ensure opts.config exists
             opts.config.header = vim.split(logo, "\n")
         end,
     },
